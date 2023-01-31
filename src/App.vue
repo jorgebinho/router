d8fd98ac37678356813fbc1094be787aaf19e22e
Partithura <jorgebrunoalmeidadossantos@gmail.com>
2023-01-31 14:27:55 -0300

    🧑‍💻 Rota usuario
    Roteamento da view Usuario conseguindo pegar  parametros da URL

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/servicos">Serviços</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/usuarios/10">Usuario</router-link>
   </nav>
   <router-view/>
 </template>
